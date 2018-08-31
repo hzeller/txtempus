8e6cf683faafd116f23413c8ae7bed7f46fdce24
Henner Zeller <h.zeller@acm.org>
2018-08-31 08:30:46 -0700

    Fix indentation.

--- dcf77-source.cc
+++ dcf77-source.cc
@@ -25,31 +25,31 @@ static uint64_t parity(uint64_t d, uint8_t from, uint8_t to_including) {
 }
 
 void DCF77TimeSignalSource::PrepareMinute(time_t t) {
-    t += 60;  // We're sending the _upcoming_ minute.
-    struct tm breakdown;
-    localtime_r(&t, &breakdown);
+  t += 60;  // We're sending the _upcoming_ minute.
+  struct tm breakdown;
+  localtime_r(&t, &breakdown);
 
-    // https://de.wikipedia.org/wiki/DCF77
-    time_bits_ = 0;
-    time_bits_ |= (breakdown.tm_isdst ? 1 : 0) << 17;
-    time_bits_ |= (breakdown.tm_isdst ? 0 : 1) << 18;
-    time_bits_ |= (1<<20);  // start time bit.
-    time_bits_ |= to_bcd(breakdown.tm_min)  << 21;
-    time_bits_ |= to_bcd(breakdown.tm_hour) << 29;
-    time_bits_ |= to_bcd(breakdown.tm_mday) << 36;
-    time_bits_ |= to_bcd(breakdown.tm_wday ? breakdown.tm_wday : 7) << 42;
-    time_bits_ |= to_bcd(breakdown.tm_mon + 1) << 45;
-    time_bits_ |= to_bcd(breakdown.tm_year % 100) << 50;
+  // https://de.wikipedia.org/wiki/DCF77
+  time_bits_ = 0;
+  time_bits_ |= (breakdown.tm_isdst ? 1 : 0) << 17;
+  time_bits_ |= (breakdown.tm_isdst ? 0 : 1) << 18;
+  time_bits_ |= (1<<20);  // start time bit.
+  time_bits_ |= to_bcd(breakdown.tm_min)  << 21;
+  time_bits_ |= to_bcd(breakdown.tm_hour) << 29;
+  time_bits_ |= to_bcd(breakdown.tm_mday) << 36;
+  time_bits_ |= to_bcd(breakdown.tm_wday ? breakdown.tm_wday : 7) << 42;
+  time_bits_ |= to_bcd(breakdown.tm_mon + 1) << 45;
+  time_bits_ |= to_bcd(breakdown.tm_year % 100) << 50;
 
-    time_bits_ |= parity(time_bits_, 21, 27) << 28;
-    time_bits_ |= parity(time_bits_, 29, 34) << 35;
-    time_bits_ |= parity(time_bits_, 36, 57) << 58;
+  time_bits_ |= parity(time_bits_, 21, 27) << 28;
+  time_bits_ |= parity(time_bits_, 29, 34) << 35;
+  time_bits_ |= parity(time_bits_, 36, 57) << 58;
 }
 
 TimeSignalSource::SecondModulation
 DCF77TimeSignalSource::GetModulationForSecond(int second) {
-    if (second >= 59)
-        return {{CarrierPower::HIGH, 0}};  // Synchronization
-    const bool bit = time_bits_ & (1LL << second);
-    return {{CarrierPower::LOW, bit ? 200 : 100}, {CarrierPower::HIGH, 0}};
+  if (second >= 59)
+    return {{CarrierPower::HIGH, 0}};  // Synchronization
+  const bool bit = time_bits_ & (1LL << second);
+  return {{CarrierPower::LOW, bit ? 200 : 100}, {CarrierPower::HIGH, 0}};
 }
--- wwvb-source.cc
+++ wwvb-source.cc
@@ -18,36 +18,36 @@
 // WWVB uses BCD, but always has a zero bit between the digits.
 // So let's call it 'padded' BCD.
 static uint64_t to_padded5_bcd(int n) {
-    return (((n / 100) % 10) << 10) | (((n / 10) % 10) << 5) | (n % 10);
+  return (((n / 100) % 10) << 10) | (((n / 10) % 10) << 5) | (n % 10);
 }
 
 static uint64_t is_leap_year(int year) {
-    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+  return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
 
 void WWVBTimeSignalSource::PrepareMinute(time_t t) {
-    struct tm breakdown;
-    gmtime_r(&t, &breakdown);  // Time transmission is always in UTC.
+  struct tm breakdown;
+  gmtime_r(&t, &breakdown);  // Time transmission is always in UTC.
 
-    // https://en.wikipedia.org/wiki/WWVB
-    // The WWVB format uses Bit-Bigendianess, so we'll start with the first
-    // bit left in our integer in bit 59.
-    time_bits_ = 0;  // All the unused bits are zero.
-    time_bits_ |= to_padded5_bcd(breakdown.tm_min) << (59 - 8);
-    time_bits_ |= to_padded5_bcd(breakdown.tm_hour) << (59 - 18);
-    time_bits_ |= to_padded5_bcd(breakdown.tm_yday + 1) << (59 - 33);
-    time_bits_ |= to_padded5_bcd(breakdown.tm_year % 100) << (59 - 53);
-    time_bits_ |= is_leap_year(breakdown.tm_year + 1900) << (59 - 55);
+  // https://en.wikipedia.org/wiki/WWVB
+  // The WWVB format uses Bit-Bigendianess, so we'll start with the first
+  // bit left in our integer in bit 59.
+  time_bits_ = 0;  // All the unused bits are zero.
+  time_bits_ |= to_padded5_bcd(breakdown.tm_min) << (59 - 8);
+  time_bits_ |= to_padded5_bcd(breakdown.tm_hour) << (59 - 18);
+  time_bits_ |= to_padded5_bcd(breakdown.tm_yday + 1) << (59 - 33);
+  time_bits_ |= to_padded5_bcd(breakdown.tm_year % 100) << (59 - 53);
+  time_bits_ |= is_leap_year(breakdown.tm_year + 1900) << (59 - 55);
 
-    // Need local time to determine DST status. TODO: announcement bits.
-    localtime_r(&t, &breakdown);
-    time_bits_ |= (breakdown.tm_isdst ? 0x03 : 0x00) << (59 - 58);
+  // Need local time to determine DST status. TODO: announcement bits.
+  localtime_r(&t, &breakdown);
+  time_bits_ |= (breakdown.tm_isdst ? 0x03 : 0x00) << (59 - 58);
 }
 
 TimeSignalSource::SecondModulation
 WWVBTimeSignalSource::GetModulationForSecond(int sec) {
-    if (sec == 0 || sec % 10 == 9 || sec > 59)
-        return {{CarrierPower::LOW, 800}, {CarrierPower::HIGH, 0}};
-    const bool bit = time_bits_ & (1LL << (59 - sec));
-    return {{CarrierPower::LOW, bit ? 500 : 200}, {CarrierPower::HIGH, 0}};
+  if (sec == 0 || sec % 10 == 9 || sec > 59)
+    return {{CarrierPower::LOW, 800}, {CarrierPower::HIGH, 0}};
+  const bool bit = time_bits_ & (1LL << (59 - sec));
+  return {{CarrierPower::LOW, bit ? 500 : 200}, {CarrierPower::HIGH, 0}};
 }
