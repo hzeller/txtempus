103e58208a943f4e5b5351941d17dee2c5a680a5
Henner Zeller <h.zeller@acm.org>
2018-09-03 11:11:50 -0700

    Add GPL 3.0 COPYING and make file headers consistent with that.

--- txtempus.cc
+++ txtempus.cc
@@ -1,9 +1,11 @@
 // -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+// This is txtempus, a LF time signal transmitter.
 // Copyright (C) 2018 Henner Zeller <h.zeller@acm.org>
 //
-// This program is free software; you can redistribute it and/or modify
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation version 2.
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,7 +13,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://gnu.org/licenses/gpl-2.0.txt>
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 // Time-signal simulating transmitter, supporting various types such
 // as DCF77, WWVB, ... to be run on the Raspberry Pi.
